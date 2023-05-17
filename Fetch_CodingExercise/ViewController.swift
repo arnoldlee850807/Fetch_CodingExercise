c26d1e8ae45cb25c40729df4c1bf95ec20a4cc54
Arnold Lee <arnold.kn.lee@gmail.com>
2023-05-17 16:35:02 -0700

    Initial Commit

---
+++ Fetch_CodingExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fetch_CodingExercise
+//
+//  Created by Arnold Lee on 2023/5/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
